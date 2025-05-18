b1d0a10828fa0f9df1f9f8011ced42aae71eab50
Bisrat Gizaw <bisrojc60@gmail.com>
2025-05-18 22:10:30 +0300

    Update perfomance

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,1 +1,15 @@
--- Task 4: Complex Booking Query
+-- Initial complex query (to be optimized)
+SELECT 
+    b.*,
+    u.name as user_name,
+    u.email as user_email,
+    p.title as property_title,
+    p.location as property_location,
+    pay.amount,
+    pay.payment_method
+FROM bookings b
+JOIN users u ON b.user_id = u.id
+JOIN properties p ON b.property_id = p.id
+LEFT JOIN payments pay ON b.id = pay.booking_id
+WHERE b.start_date BETWEEN '2025-01-01' AND '2025-12-31'
+ORDER BY b.created_at DESC;
