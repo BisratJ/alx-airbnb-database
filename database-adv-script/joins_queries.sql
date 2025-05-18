fbc48174800763bf6ae68a90c80c259b89fb3a1d
Bisrat Gizaw <bisrojc60@gmail.com>
2025-05-18 22:08:26 +0300

    updates on join

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,19 @@
--- Task 0: Complex Joins Queries
+-- 1. INNER JOIN: All bookings with respective users
+SELECT b.*, u.*
+FROM bookings b
+INNER JOIN users u ON b.user_id = u.id;
+
+-- 2. LEFT JOIN: All properties with their reviews (including properties with no reviews)
+SELECT p.*, r.*
+FROM properties p
+LEFT JOIN reviews r ON p.id = r.property_id;
+
+-- 3. FULL OUTER JOIN (emulated in MySQL): All users and all bookings
+SELECT u.*, b.*
+FROM users u
+LEFT JOIN bookings b ON u.id = b.user_id
+UNION
+SELECT u.*, b.*
+FROM users u
+RIGHT JOIN bookings b ON u.id = b.user_id
+WHERE u.id IS NULL;
